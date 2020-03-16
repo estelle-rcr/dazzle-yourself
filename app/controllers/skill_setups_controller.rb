8f1c3469262dde017b6d6cfa3093127e1d94716a
123Charlotte <favier.cha@gmail.com>
2020-03-16 19:41:50 +0100

    users can now add and update their skills in user#edit

---
+++ app/controllers/skill_setups_controller.rb
@@ -0,0 +1,33 @@
+class SkillSetupsController < ApplicationController
+
+  def create
+    @skill_setup = SkillSetup.create!(
+      user: User.find(params[:user_id]),
+      skill: Skill.find(params[:skill_id]),
+      primary: params[:primary]
+      )
+
+    if @skill_setup.save
+      flash[:success] = "Compétence principale actualisée !"
+      redirect_to user_path(current_user.id)
+    end
+
+  end
+
+  def update
+    @skill_setup_primary = SkillSetup.find_by(user: current_user, primary: true)
+    @skill_setup_secondary = SkillSetup.find_by(user: current_user, primary: false)
+
+    if params[:primary] == "true"
+      @skill_setup_primary.update(skill: Skill.find(params[:skill_id]))
+    else
+      @skill_setup_secondary.update(skill: Skill.find(params[:skill_id]))
+    end
+
+    if @skill_setup_primary.save || @skill_setup_secondary.save
+      flash[:success] = "Compétence principale actualisée !"
+      redirect_to user_path(current_user.id)
+
+    end
+  end
+end
