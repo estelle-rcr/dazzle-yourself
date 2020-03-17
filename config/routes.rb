95e08a0f6755f019dae79553a4d7ee555458f6dd
IalyRambe <rambeloson.ialyntsoa@gmail.com>
2020-03-17 17:03:54 +0100

    40-Homepage-Routes and pagelist fixed

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,5 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :projects, only: [:index]
-  root to: "static#home"
-  get 'home', to:'static#home'
+  root to: 'static#home'
 end
