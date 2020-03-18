ed68ff96fc6dc4390014d4165b4070429dfc3de3
123Charlotte <favier.cha@gmail.com>
2020-03-18 12:01:03 +0100

    create and edit project methods and views

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,5 @@
 class ProjectsController < ApplicationController
+
   def index
 
   end
@@ -9,18 +10,10 @@ class ProjectsController < ApplicationController
 
 
   def create
-    @packages = Package.all
+
+    @project = Project.create(project_params)
     @start_date = Time.parse(params[:project].to_s)
-    
-    @project = Project.new(
-      owner: current_user,
-      package_id: params[:package_id],
-      title: params[:title],
-      short_description: params[:short_description],
-      long_description: params[:long_description],
-      start_date: @start_date,
-      attendees_goal: params[:attendees_goal],
-      state: "draft")
+    @project.update(start_date: @start_date, state: "draft")
 
     if @project.save
       flash[:success] = "Le projet a été créé !"
@@ -31,6 +24,39 @@ class ProjectsController < ApplicationController
    end
  end
 
- 
+ def edit
+  @packages = Package.all
+  @project = Project.find(params[:id])
+end
+
+def update
+  @packages = Package.all
+  @project = Project.find(params[:id])
+  @project.update(project_params)
+  @start_date = Time.parse(params[:project].to_s)
+
+  if params[:publish] == "Soumettre mon projet"
+    @project.update(start_date: @start_date, state: "submitted")
+  else
+    @project.update(start_date: @start_date)
+  end
+
+  if @project.save
+    flash[:success] = "Le projet a été modifié !"
+    redirect_to projects_path
+  else
+   flash.now[:error] = @project.errors.full_messages.to_sentence
+   render :new
+ end
+
+end
+
+private
+
+def project_params
+  params.permit(:owner_id, :package_id, :title, :short_description, :long_description, :attendees_goal)
+end
+
+
 
 end
