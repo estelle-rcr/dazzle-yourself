473997dd620476d681ad3aaf70d5dfb85f698eac
123Charlotte <favier.cha@gmail.com>
2020-03-18 17:52:37 +0100

    owner can pay their project

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,4 +1,6 @@
 class ChargesController < ApplicationController
+  before_action :my_project
+  
   def new
     @project = Project.find(params[:project_id])
     @amount = @project.package.price_owner
@@ -13,7 +15,7 @@ class ChargesController < ApplicationController
       source: params[:stripeToken],
     })
 
-    charge = Stripe::Charge.create({
+    @charge = Stripe::Charge.create({
       customer: customer.id,
       amount: @amount,
       description: "Paiement de l'inscription",
@@ -28,4 +30,13 @@ class ChargesController < ApplicationController
     redirect_to new_charge_path
   end
 
+  private
+
+  def my_project
+    @project = Project.find(params[:project_id])
+    unless current_user == @project.owner
+      flash[:error] ="Vous n'êtes pas autorisé à consulter cette page"
+      redirect_to root_path
+    end
+  end
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,5 @@
 class ProjectsController < ApplicationController
-
+before_action :my_project, only: [:edit, :update]
 
   def index
     @projects = Project.all
@@ -22,7 +22,7 @@ class ProjectsController < ApplicationController
 
     if @project.save
       flash[:success] = "Le projet a été créé !"
-      redirect_to projects_path
+      redirect_to project_path(@project.id)
     else
      flash.now[:error] = @project.errors.full_messages.to_sentence
      render :new
@@ -43,20 +43,20 @@ def update
   if params[:publish] == "Soumettre mon projet"
     @project.update(start_date: @start_date, state: "submitted", submit_date: Time.now)
     if @project.save
-      flash[:danger] = "Pour que le projet soit publié, le paiement doit être effectué"
+      flash[:alert] = "Pour que le projet soit publié, le paiement doit être effectué"
       redirect_to new_project_charge_path(@project.id)
     else
      flash.now[:error] = @project.errors.full_messages.to_sentence
-     render :new
+     render :edit
    end
  else
   @project.update(start_date: @start_date)
   if @project.save
     flash[:success] = "Le projet a été modifié !"
-    redirect_to projects_path
+    redirect_to project_path(@project.id)
   else
    flash.now[:error] = @project.errors.full_messages.to_sentence
-   render :new
+   render :edit
  end
 end
 
@@ -68,6 +68,14 @@ private
 
 def project_params
   params.permit(:owner_id, :package_id, :title, :short_description, :long_description, :attendees_goal)
+end
+
+def my_project
+  @project = Project.find(params[:id])
+  unless current_user == @project.owner
+    flash[:error] ="Vous n'êtes pas autorisé à consulter cette page"
+      redirect_to root_path
+  end
 end
 
 
