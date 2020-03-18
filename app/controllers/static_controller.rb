8f4b246e1a1f6513ecdc0b9a072df431cb893683
BDiumula <diumula.benedicte@hotmail.fr>
2020-03-18 11:12:45 +0100

    pimp devise forms

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,14 @@
 class StaticController < ApplicationController
+  before_action :authenticate_user!, only: [:secret]
+   
     def home
+    end
+
+    def index 
+
+    end 
+
+    def secret 
+
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
     resources :skill_setups
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :projects, only: [:index]
+  resources :projects, only: [:index, :show]
   root to: 'static#home'
 end
