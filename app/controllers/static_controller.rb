9cbdeed89bec68e3636050d807d788568419748f
IalyRambe <rambeloson.ialyntsoa@gmail.com>
2020-03-19 17:24:34 +0100

    40 homepage jumbotron,cards and CTO reviewed

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,10 +2,11 @@ class StaticController < ApplicationController
   before_action :authenticate_user!, only: [:secret]
    
     def home
+      @projects = Project.all
     end
 
     def index 
-
+      
     end 
 
     def secret 
