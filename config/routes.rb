31d02b1d8eb8c3f7f630b2c0c9ce77a5ca575ceb
carolin.labinski <carolin.labinski@yahoo.de>
2020-03-19 09:55:46 +0100

    cards, jumbotron

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :projects, only: [:index]
   root to: 'static#home'
+  get 'homepage', to:'static#homepage'
 end
