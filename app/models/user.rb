5d7e799672cc5606078051ae0656412dbe129f26
123Charlotte <favier.cha@gmail.com>
2020-03-20 09:54:29 +0100

    method for emails paused

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,9 @@ class User < ApplicationRecord
 
   after_create :welcome_send
 
-  def welcome_send
-    UserMailer.welcome_email(self).deliver_now
-  end
+  # def welcome_send
+  #   UserMailer.welcome_email(self).deliver_now
+  # end
 
   
 end
