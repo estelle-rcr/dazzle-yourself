75508b9240115cfc0e61e75ba21842b0502125e9
123Charlotte <favier.cha@gmail.com>
2020-03-20 14:47:26 +0100

    fix skills form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   
   def show
-    @user = User.find(params[:id])
     @user = current_user 
     @attendances = Attendance.where(attendee: current_user) 
     @projects = Project.where(owner: current_user) 
--- db/migrate/20200318094252_default_value_state_project.rb
+++
@@ -1,4 +0,0 @@
-class DefaultValueStateProject < ActiveRecord::Migration[5.2]
-  def change
-  end
-end
