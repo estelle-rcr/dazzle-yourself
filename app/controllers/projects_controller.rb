8614aefae7be591644a9e48ce826af723c450874
IalyRambe <59255116+IalyRambe@users.noreply.github.com>
2020-03-20 16:33:26 +0100

    update projects_controller with skill_attendee

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -9,7 +9,7 @@ class ProjectsController < ApplicationController
 
   def show
     @project = Project.find(params[:id])
-    @skill_setup_primary = SkillSetup.find_by(user: current_user, primary: true)
+    @skill_setup_primary = SkillSetup.find_by(user: attendee, primary: true)
   end
 
   
