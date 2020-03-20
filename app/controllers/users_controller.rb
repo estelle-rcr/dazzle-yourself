620b4c564b28c37ae04d2506cfd162509d892350
123Charlotte <favier.cha@gmail.com>
2020-03-20 15:03:16 +0100

    fix skills

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,8 @@ class UsersController < ApplicationController
   def show
     @user = current_user 
     @attendances = Attendance.where(attendee: current_user) 
-    @projects = Project.where(owner: current_user) 
+    @projects = Project.where(owner: current_user)
+    @skills = Skill.all
     @skill_setup_primary = SkillSetup.find_by(user: current_user, primary: true)
     @skill_setup_secondary = SkillSetup.find_by(user: current_user, primary: false)
   end
