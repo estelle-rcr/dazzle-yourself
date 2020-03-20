93bfcbde185583f5d25b8cb950058abc0ebdc280
carolin.labinski <carolin.labinski@yahoo.de>
2020-03-20 19:24:52 +0100

    add packages prices and CTA partial

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,11 +1,13 @@
 class StaticController < ApplicationController
-    def homepage
-      render layout: "layouts/application_homepage"
-      @packages = Package.all
-    end
-
-    def home
-      @projects = Project.all
-	    render layout: "layouts/application_home"
-    end
+  def home
+    @projects = Project.all
+  render layout: "layouts/application_home"
+  end
+  def homepage
+    @packages = Package.all
+  end
+  
 end
+
+
+
