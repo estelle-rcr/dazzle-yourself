427213b81b4e8a5471d208c9d4722849763a53ab
Estelle Recuero <estelle-ad@outlook.com>
2020-03-30 01:01:14 +0200

    debug app for heroku deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -94,4 +94,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.secret_key_base = ENV['SECRET_KEY_RAILS']
+
+  config.assets.initialize_on_precompile = false
 end
