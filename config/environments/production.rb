8856137651f2cf3fc4a2770d821ea84344fe88de
Estelle Recuero <estelle-ad@outlook.com>
2020-03-29 23:14:04 +0200

    config production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.secret_key_base = ENV['SECRET_KEY_RAILS']
+
+  RAILS_ENV=production bundle exec rake assets:precompile
 end
